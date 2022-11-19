a52590e1d15dac152fcb0dbb05d51541ba786705
Shivam <chandrakant.kushwah2019@sait.ac.in>
2022-11-19 16:59:55 +0530

    Page not found designed and updated

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,27 @@
 import React from "react";
+import SvgPNF from "../images/svgPNF";
+// routing
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="bg-blue-400 text-white pb-5 ">
+      <div className="w-10/12 mx-auto">
+        <h1 className="text-center pt-12 text-4xl font-semibold">
+          Looks like you're lost, Odinite!
+        </h1>
+        <h5 className="text-center text-xl font-medium mt-5 ">
+          Let's get back to{" "}
+          <Link to="/">
+            <span className="hover:text-gold">HOME</span>
+          </Link>
+        </h5>
+      </div>
+      <div className="md:w-2/3 mx-auto">
+        <SvgPNF />
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
